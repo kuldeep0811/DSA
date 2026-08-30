92dd4fa371e8312072dbfc3dfab23181eee6d49a
kuldeep singh <kuldeepcktd9748@gmail.com>
2026-08-30 17:46:57 +0530

    Time: 4 ms (19.64%) | Memory: 263.4 MB (10.31%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,42 @@
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+      
+       
+    //     int n=nums.size();
+    //       k=k%n;
+          
+    //     for(int i=0;i<k;i++){
+    //         int last=nums[n-1];
+        
+    //       for(int j=n-1;j>0;j--){
+             
+             
+    //         nums[j]=nums[j-1];
+
+    //       }
+    //       nums[0]=last;
+    //     }
+        
+    // }
+   
+
+      
+   class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        k = k % n;
+
+        reverse(nums.begin(), nums.end());
+
+        reverse(nums.begin(), nums.begin() + k);
+
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+  
+
+// };
