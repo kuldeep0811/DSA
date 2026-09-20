effc39d376ba5933705b1aa61981c2b65ddf4417
kuldeep singh <kuldeepcktd9748@gmail.com>
2026-09-20 20:58:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (41.41%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.c
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.c
@@ -4,7 +4,9 @@ int reverseDegree(char* s) {
     
     int num=0;
     for(int i=0;i<strlen(s);i++){
-        int reverse='z'-s[i]+1;
+        int n=s[i]-'a'+1;
+        int reverse=26-n+1;
+
         int x=(i+1);
         num=num+reverse*x;
 
